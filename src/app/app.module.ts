d8e27dc6d1c5b0354486de5a675bc03c22f4ed3e
= <garreffd92@gmail.com>
2019-10-29 22:22:43 -0400

    Add todo list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//Remember to add the HttpClientModule in imports to make it accessible everywhere.
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from '../models/todo';
+
+import {TodoService} from '../todo.service';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+
+  todos : Todo[];
+
+/*
+  Upon instantiation Angular will use its DI system to set todoService to a singleton instance of TodoService.
+*/
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+    console.log(this.todoService);
+    //this.getTodos();
+  }
+
+  getTodos() : void {
+    //Revist the => function
+    //This statement will wait for Observable to emit an array of todos.
+    this.todoService.getTodos().subscribe(todos => this.todos = todos);
+  }
+
+}
