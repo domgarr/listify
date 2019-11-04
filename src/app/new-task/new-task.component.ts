6cbf96399c5e177eeb72df7e31b6ddea5a783e1c
= <garreffd92@gmail.com>
2019-11-03 23:40:36 -0500

    Add new task icon renders input when clicked and input disappears when not adding tasks

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,Output, ViewChild, EventEmitter, NgModule, ElementRef} from '@angular/core';
+import { Component, OnInit, Input,Output, ViewChild, EventEmitter, NgModule, ElementRef, ChangeDetectorRef} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import {Task} from '../models/task';
@@ -11,12 +11,14 @@ import {Task} from '../models/task';
 
 export class NewTaskComponent implements OnInit {
   description : string;
+  addingNewTask : boolean;
   //static:false : set it to True to resolve query results before change detection runs
   @ViewChild('inputTask', {static:false}) inputTask : ElementRef;
 
   @Output() newTaskAdded = new EventEmitter<Task>();
 
-  constructor() {
+  constructor(private ref : ChangeDetectorRef) {
+    this.addingNewTask = false;
   }
 
   ngOnChanges(chg){
@@ -50,6 +52,17 @@ export class NewTaskComponent implements OnInit {
     //Set textbox to empty
     this.description = "";
     console.log(this);
+    }
+
+    onClickAddIcon(){
+      this.addingNewTask = true;
+      this.ref.detectChanges();
+      this.inputTask.nativeElement.focus();
+    }
+
+    focusOffInputTask(){
+      this.addingNewTask = false;
+      this.description ="";
     }
 
 
