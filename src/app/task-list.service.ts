03ae027132fafcbc335d544dceb9ce676e23fa8b
= <garreffd92@gmail.com>
2019-11-30 01:09:11 -0500

    Fix bug in TaskListService where Auth Token was updating in immutable header object

--- src/app/task-list.service.ts
+++ src/app/task-list.service.ts
@@ -9,7 +9,7 @@ import {TaskList} from './models/task-list';
 })
 export class TaskListService {
   //If this service is called - it has already passed the token_id check.
-  readonly httpOptions = {
+  httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json',
       'Authorization': localStorage.getItem("id_token")
@@ -20,19 +20,34 @@ export class TaskListService {
 
   constructor(private http : HttpClient) { }
 
+  updateHeader(){
+    /* When updating headers it is very important to use set() which will return a clone
+    of the current header with the change applied! This is because HttpHeader class is immutable.
+
+    Updating headers
+    https://angular.io/guide/http
+    */
+
+    this.httpOptions.headers = this.httpOptions.headers.set("Authorization", localStorage.getItem("id_token"));
+  }
+
   saveTaskList(taskList : TaskList) : Observable<TaskList> {
+    this.updateHeader();
     return this.http.post<TaskList>(this.taskListUrl, taskList, this.httpOptions);
   }
 
   getAllTaskLists() : Observable<TaskList[]> {
+    this.updateHeader();
     return this.http.get<TaskList[]>(this.taskListUrl, this.httpOptions);
   }
 
   updateTaskListName(taskList : object): Observable<any> {
+    this.updateHeader();
     return this.http.put(this.taskListUrl, taskList, this.httpOptionsIncludingObserveResponse());
   }
 
   deleteTaskList(listId): Observable<any>  {
+    this.updateHeader();
     return this.http.delete(this.taskListUrl + '/' + listId,  this.httpOptionsIncludingObserveResponse());
   }
 
